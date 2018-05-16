9f6719f3f1717a65b611c44b61726f867c61465e
Andre Polo Norte <apolonorte@gmail.com>
2018-05-16 19:09:42 -0300

    comando delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -125,6 +125,21 @@ class Usuario {
 		));
 	}
 
+	public function delete(){
+
+		$sql = new sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdusuario(0);	
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+
+	}
+
 	public function __construct($login = "", $password = ""){
 
 		$this->setDeslogin($login);
--- index.php
+++ index.php
@@ -30,9 +30,15 @@ echo json_encode($usuarios);
 //$aluno->insert();
 //echo $aluno;
 
+//Alterar um usuário
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//$usuario->update("professor", "!@#$%¨&*");
+//echo $usuario;
+
 $usuario = new Usuario();
-$usuario->loadById(8);
-$usuario->update("professor", "!@#$%¨&*");
+$usuario->loadById(7);
+$usuario->delete();
 echo $usuario;
 
  ?>
